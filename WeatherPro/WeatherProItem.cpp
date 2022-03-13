2db838c093da4e60c3fe8b1c02ef286b70157f6e
haojia <haojia521@hotmail.com>
2022-03-14 00:42:30 +0800

    primary codes of item class

---
+++ WeatherPro/WeatherProItem.cpp
@@ -0,0 +1,46 @@
+﻿#include "pch.h"
+#include "WeatherProItem.h"
+
+const wchar_t* CWeatherProItem::GetItemName() const
+{
+    static CString itemName;
+    if (itemName.IsEmpty()) itemName.LoadStringW(IDS_WEATHER_PRO);
+
+    return itemName;
+}
+
+const wchar_t* CWeatherProItem::GetItemId() const
+{
+    return L"FxOP34Lm";
+}
+
+const wchar_t* CWeatherProItem::GetItemLableText() const
+{
+    return L"";
+}
+
+const wchar_t* CWeatherProItem::GetItemValueText() const
+{
+    return L"";
+}
+
+const wchar_t* CWeatherProItem::GetItemValueSampleText() const
+{
+    return L"";
+}
+
+bool CWeatherProItem::IsCustomDraw() const
+{
+    return true;
+}
+
+int CWeatherProItem::GetItemWidthEx(void* hDC) const
+{
+    // todo
+    return 0;
+}
+
+void CWeatherProItem::DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode)
+{
+    // todo
+}
---
+++ WeatherPro/WeatherProItem.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <include/PluginInterface.h>
+
+class CWeatherProItem : public IPluginItem
+{
+public:
+    const wchar_t* GetItemName() const override;
+    const wchar_t* GetItemId() const override;
+    const wchar_t* GetItemLableText() const override;
+    const wchar_t* GetItemValueText() const override;
+    const wchar_t* GetItemValueSampleText() const override;
+    bool IsCustomDraw() const override;
+    int GetItemWidthEx(void* hDC) const override;
+    void DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode) override;
+};
