46eb4311f9587df6909b9978949b77b84eb28a06
caiyuhong <cai1083088795@gmail.com>
2022-03-16 14:41:59 +0800

    修改界面

--- GP/resource.h
+++ GP/resource.h
@@ -12,6 +12,10 @@
 #define IDC_FULL_DAY_CHECK              1005
 #define IDC_CODE_LABEL                  1006
 #define IDC_RADIO1                      1013
+#define IDC_RADIO2                      1014
+#define IDC_RADIO3                      1015
+#define IDC_RADIO4                      1016
+#define IDC_RADIO5                      1017
 #define ID_SETTINGS                     40003
 #define ID_UPDATE                       40004
 #define ID_OPTIONS                      40005
@@ -23,7 +27,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40007
-#define _APS_NEXT_CONTROL_VALUE         1014
+#define _APS_NEXT_CONTROL_VALUE         1017
 #define _APS_NEXT_SYMED_VALUE           103
 #endif
 #endif
