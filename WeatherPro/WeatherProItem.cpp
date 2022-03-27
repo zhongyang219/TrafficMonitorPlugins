943b8a59b83426172ed1462ce36e0b552d9be991
haojia <haojia521@hotmail.com>
2022-03-27 23:09:38 +0800

    初步实现文本绘制

--- WeatherPro/WeatherProItem.cpp
+++ WeatherPro/WeatherProItem.cpp
@@ -1,12 +1,10 @@
 ﻿#include "pch.h"
 #include "WeatherProItem.h"
+#include "DataManager.h"
 
 const wchar_t* CWeatherProItem::GetItemName() const
 {
-    static CString itemName;
-    if (itemName.IsEmpty()) itemName.LoadStringW(IDS_WEATHER_PRO);
-
-    return itemName;
+    return CDataManager::InstanceRef().StringRes(IDS_WEATHER_PRO);
 }
 
 const wchar_t* CWeatherProItem::GetItemId() const
@@ -36,11 +34,25 @@ bool CWeatherProItem::IsCustomDraw() const
 
 int CWeatherProItem::GetItemWidthEx(void* hDC) const
 {
-    // todo
-    return 0;
+    CDC* pDC = CDC::FromHandle((HDC)hDC);
+
+    auto icon_width = CDataManager::Instance().GetConfig().m_show_weather_icon ? CDataManager::Instance().DPI(20) : 0;
+
+    return icon_width + pDC->GetTextExtent(CDataManager::Instance().GetWeatherTemperature().c_str()).cx;
 }
 
 void CWeatherProItem::DrawItem(void* hDC, int x, int y, int w, int h, bool dark_mode)
 {
-    // todo
+    CDC* pDC = CDC::FromHandle((HDC)hDC);
+
+    CRect rect(CPoint(x, y), CSize(w, h));
+
+    if (CDataManager::Instance().GetConfig().m_show_weather_icon)
+    {
+        // todo: draw icon
+
+        rect.left += CDataManager::Instance().DPI(20);
+    }
+
+    pDC->DrawText(CDataManager::Instance().GetWeatherTemperature().c_str(), rect, DT_VCENTER | DT_SINGLELINE | DT_NOPREFIX);
 }
