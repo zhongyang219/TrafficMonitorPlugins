1ba250d8aa1159fb2a776f5a929c6d8626edabb9
haojia <haojia521@hotmail.com>
2022-03-27 23:09:11 +0800

    实现天气信息更新，加载和保存参数设置

--- WeatherPro/DataManager.cpp
+++ WeatherPro/DataManager.cpp
@@ -2,6 +2,19 @@
 #include "DataManager.h"
 #include "resource.h"
 
+#include <sstream>
+#include <chrono>
+#include <thread>
+
+SConfiguration::SConfiguration() :
+    m_wit(EWeatherInfoType::WEATHER_REALTIME),
+    m_show_weather_icon(true),
+    m_show_weather_in_tooltips(true),
+    m_show_brief_rt_weather_info(false),
+    m_show_weather_alerts(true),
+    m_show_brief_weather_alert_info(true)
+{}
+
 CDataManager CDataManager::m_instance;
 
 CDataManager::CDataManager()
@@ -14,7 +27,12 @@ CDataManager::CDataManager()
 CDataManager::~CDataManager()
 {}
 
-CDataManager& CDataManager::Instance()
+const CDataManager& CDataManager::Instance()
+{
+    return m_instance;
+}
+
+CDataManager& CDataManager::InstanceRef()
 {
     return m_instance;
 }
@@ -43,4 +61,169 @@ float CDataManager::DPIF(float pixel) const
 int CDataManager::RDPI(int pixel) const
 {
     return pixel * 96 / m_dpi;
+}
+
+void CDataManager::SetCurrentCityInfo(SCityInfo info)
+{
+    m_currentCityInfo = info;
+}
+
+const SCityInfo& CDataManager::GetCurrentCityInfo() const
+{
+    return m_currentCityInfo;
+}
+
+void CDataManager::_updateWeather()
+{
+    bool flag{ false };
+    int query_times{ 0 };
+
+    do 
+    {
+        if (query_times > 0)
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        flag = query::QueryRealTimeWeather(GetCurrentCityInfo().CityNO, m_rt_weather);
+
+        ++query_times;
+    } while (!flag && query_times <=3);
+
+    flag = false;
+    query_times = 0;
+
+    do 
+    {
+        if (query_times > 0)
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        flag = query::QueryWeatherAlerts(GetCurrentCityInfo().CityNO, m_weather_alerts);
+
+        ++query_times;
+    } while (!flag && query_times <= 3);
+
+    flag = false;
+    query_times = 0;
+
+    do 
+    {
+        if (query_times > 0)
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        flag = query::QueryForecastWeather(GetCurrentCityInfo().CityNO, m_weather_today, m_weather_tommrow);
+
+        ++query_times;
+    } while (!flag && query_times <= 3);
+}
+
+void CDataManager::UpdateWeather()
+{
+    std::thread t(&CDataManager::_updateWeather, this);
+    t.detach();
+}
+
+std::wstring CDataManager::GetWeatherTemperature() const
+{
+    switch (m_config.m_wit)
+    {
+    case EWeatherInfoType::WEATHER_REALTIME:
+        if (m_config.m_show_weather_icon)
+            return m_rt_weather.GetTemperature();
+        else
+            return m_rt_weather.Weather + L" " + m_rt_weather.GetTemperature();
+
+    case EWeatherInfoType::WEATHER_TODAY:
+        if (m_config.m_show_weather_icon)
+            return m_weather_today.GetTemperature();
+        else
+            return m_weather_today.ToString();
+
+    case EWeatherInfoType::WEATHER_TOMMROW:
+        if (m_config.m_show_weather_icon)
+            return m_weather_tommrow.GetTemperature();
+        else
+            return m_weather_tommrow.ToString();
+
+    default:
+        return L"-";
+    }
+}
+
+std::wstring CDataManager::GetRealTimeWeatherInfo(bool brief) const
+{
+    return m_rt_weather.ToString(brief);
+}
+
+std::wstring CDataManager::GetWeatherAlertsInfo(bool brief) const
+{
+    if (m_weather_alerts.empty())
+    {
+        return std::wstring();
+    }
+    else
+    {
+        std::wstringstream wss;
+
+        for (const auto &alert : m_weather_alerts)
+            wss << alert.ToString(brief) << std::endl;
+
+        return wss.str();
+    }
+}
+
+std::wstring CDataManager::GetWeatherInfo() const
+{
+    std::wstringstream wss;
+
+    wss << m_weather_today.ToString() << std::endl
+        << m_weather_tommrow.ToString();
+
+    return wss.str();
+}
+
+SConfiguration& CDataManager::GetConfig()
+{
+    return m_config;
+}
+
+const SConfiguration& CDataManager::GetConfig() const
+{
+    return m_config;
+}
+
+void CDataManager::LoadConfigs(const std::wstring &cfg_dir)
+{
+    const auto &ch = cfg_dir.back();
+    if (ch == L'\\' || ch == L'/')
+        m_config_file_path = cfg_dir + L"WeatherPro.ini";
+    else
+        m_config_file_path = cfg_dir + L"\\WeatherPro.ini";
+
+    auto cfg_int_val_getter = [this](const wchar_t *key, int default_val) {
+        int val = GetPrivateProfileInt(L"config", key, default_val, m_config_file_path.c_str());
+        return val;
+    };
+
+    m_config.m_wit = static_cast<EWeatherInfoType>(cfg_int_val_getter(L"wit", 0));
+    m_config.m_show_weather_icon = (cfg_int_val_getter(L"show_weather_icon", 1) != 0);
+    m_config.m_show_weather_in_tooltips = (cfg_int_val_getter(L"show_weather_in_tooltips", 1) != 0);
+    m_config.m_show_brief_rt_weather_info = (cfg_int_val_getter(L"show_brief_rt_weather_info", 0) != 0);
+    m_config.m_show_weather_alerts = (cfg_int_val_getter(L"show_weather_alerts", 1) != 0);
+    m_config.m_show_brief_weather_alert_info = (cfg_int_val_getter(L"show_brief_weather_alert_info", 1) != 0);
+}
+
+void CDataManager::SaveConfigs() const
+{
+    auto cfg_int_val_writter = [this](const wchar_t *key, int value) {
+        wchar_t buffer[16]{ 0 };
+
+        swprintf_s(buffer, L"%d", value);
+        WritePrivateProfileString(L"config", key, buffer, m_config_file_path.c_str());
+    };
+
+    cfg_int_val_writter(L"wit", static_cast<int>(m_config.m_wit));
+    cfg_int_val_writter(L"show_weather_icon", m_config.m_show_weather_icon ? 1 : 0);
+    cfg_int_val_writter(L"show_weather_in_tooltips", m_config.m_show_weather_in_tooltips ? 1 : 0);
+    cfg_int_val_writter(L"show_brief_rt_weather_info", m_config.m_show_brief_rt_weather_info ? 1 : 0);
+    cfg_int_val_writter(L"show_weather_alerts", m_config.m_show_weather_alerts ? 1 : 0);
+    cfg_int_val_writter(L"show_brief_weather_alert_info", m_config.m_show_brief_weather_alert_info ? 1 : 0);
 }
--- WeatherPro/DataManager.h
+++ WeatherPro/DataManager.h
@@ -1,5 +1,25 @@
 ﻿#pragma once
 #include <map>
+#include "DataQuerier.h"
+
+enum class EWeatherInfoType
+{
+    WEATHER_REALTIME,
+    WEATHER_TODAY,
+    WEATHER_TOMMROW
+};
+
+struct SConfiguration
+{
+    SConfiguration();
+
+    EWeatherInfoType m_wit;
+    bool m_show_weather_icon;
+    bool m_show_weather_in_tooltips;
+    bool m_show_brief_rt_weather_info;
+    bool m_show_weather_alerts;
+    bool m_show_brief_weather_alert_info;
+};
 
 class CDataManager
 {
@@ -7,7 +27,8 @@ private:
     CDataManager();
     ~CDataManager();
 public:
-    static CDataManager& Instance();
+    static const CDataManager& Instance();
+    static CDataManager& InstanceRef();
 
     const CString& StringRes(UINT id);
 
@@ -15,8 +36,36 @@ public:
     float DPIF(float pixel) const;
     int RDPI(int pixel) const;
 
+    void SetCurrentCityInfo(SCityInfo info);
+    const SCityInfo& GetCurrentCityInfo() const;
+
+    void UpdateWeather();
+
+    std::wstring GetWeatherTemperature() const;
+    std::wstring GetRealTimeWeatherInfo(bool brief) const;
+    std::wstring GetWeatherAlertsInfo(bool brief) const;
+    std::wstring GetWeatherInfo() const;
+
+    SConfiguration& GetConfig();
+    const SConfiguration& GetConfig() const;
+
+    void LoadConfigs(const std::wstring &cfg_dir);
+    void SaveConfigs() const;
+    
 private:
+    void _updateWeather();
+
     static CDataManager m_instance;
     int m_dpi;
     std::map<UINT, CString> m_string_resources;
+
+    SConfiguration m_config;
+    std::wstring m_config_file_path;
+
+    SCityInfo m_currentCityInfo;
+
+    SRealTimeWeather m_rt_weather;
+    WeatherAlertList m_weather_alerts;
+    SWeatherInfo m_weather_today;
+    SWeatherInfo m_weather_tommrow;
 };
