72cf110e4e2059d99f1d3fb69ca1a32008e73972
haojia <haojia521@hotmail.com>
2022-03-29 18:37:42 +0800

    添加icon资源

--- WeatherPro/Resource.h
+++ WeatherPro/Resource.h
@@ -25,12 +25,45 @@
 #define IDC_CHECK_SHOW_WEATHER_ALERTS   1009
 #define IDC_CHECK5                      1010
 #define IDC_CHECK_SHOW_BRIEF_WEATHER_ALERT_INFO 1010
+#define IDI_ICON_B_A02                  1008
+#define IDI_ICON_B_A04                  1009
+#define IDI_ICON_B_A05                  1010
+#define IDI_ICON_B_A06                  1011
+#define IDI_ICON_B_A07                  1012
+#define IDI_ICON_B_A08                  1013
+#define IDI_ICON_B_A09                  1014
+#define IDI_ICON_B_A10                  1015
+#define IDI_ICON_B_A11                  1016
+#define IDI_ICON_B_A12                  1017
+#define IDI_ICON_B_A14                  1018
+#define IDI_ICON_B_A15                  1019
+#define IDI_ICON_B_A16                  1020
+#define IDI_ICON_B_A17                  1021
+#define IDI_ICON_B_A18                  1022
+#define IDI_ICON_B_A19                  1023
+#define IDI_ICON_B_A20                  1024
+#define IDI_ICON_B_A29                  1025
+#define IDI_ICON_B_A30                  1026
+#define IDI_ICON_B_A31                  1027
+#define IDI_ICON_B_A53                  1028
+#define IDI_ICON_B_A54                  1029
+#define IDI_ICON_B_A55                  1030
+#define IDI_ICON_B_A56                  1031
+#define IDI_ICON_B_A99                  1032
+#define IDI_ICON_B_D00                  1033
+#define IDI_ICON_B_D01                  1034
+#define IDI_ICON_B_D03                  1035
+#define IDI_ICON_B_D13                  1036
+#define IDI_ICON_B_N00                  1037
+#define IDI_ICON_B_N01                  1038
+#define IDI_ICON_B_N03                  1039
+#define IDI_ICON_B_N13                  1040
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        1008
+#define _APS_NEXT_RESOURCE_VALUE        1041
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1011
 #define _APS_NEXT_SYMED_VALUE           1000
