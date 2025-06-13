61b3288d964508b0e312e6863198ea5319eab27e
ttimasdf <opensource@rabit.pw>
2025-06-13 11:20:46 +0800

    IpAddress: strip result from ip.sb API

--- Plugins/IpAddress/IpSbProvider.cpp
+++ Plugins/IpAddress/IpSbProvider.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "IpSbProvider.h"
 #include "Common.h"
+#include "../utilities/Common.h"
 
 std::wstring CIpSbProvider::GetName() const
 {
@@ -12,6 +13,7 @@ bool CIpSbProvider::GetExternalIp(std::wstring& ip) const
     std::string result;
     if (CCommon::GetURL(L"https://api.ip.sb/ip", result, true))
     {
+        utilities::StringHelper::StringNormalize(result);
         ip = CCommon::StrToUnicode(result.c_str(), true);
         return true;
     }
