ee23fd83162e5c12281d98f0425ec6e65fc107a5
zhongyang219 <zhongyang219@hotmail.com>
2026-07-14 11:05:49 +0800

    修正天气插件鼠标提示中的温度单位缺失的问题

--- Plugins/Weather/CommonData.cpp
+++ Plugins/Weather/CommonData.cpp
@@ -6,13 +6,9 @@
 std::wstring WeatherInfo::WeatherInfo::ToString() const
 {
     std::wstringstream wss;
-    if (m_low.empty() || is_cur_weather)
-        wss << m_type << ' ' << m_high;
-    else if (m_high.empty())
-        wss << m_type << ' ' << m_low;
-    else
-        wss << m_type << ' ' << m_low << '~' << m_high;
-    wss << ' ' << m_wind;
+    wss << m_type << L' ';
+    wss << ToStringTemperature();
+    wss << L' ' << m_wind;
     return wss.str();
 }
 
